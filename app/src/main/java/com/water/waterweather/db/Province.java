9afcff7aac8c632bda74a9578b3aa51fd605c816
water <335937419@qq.com>
2017-03-20 11:46:10 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/water/waterweather/db/Province.java
@@ -0,0 +1,40 @@
+package com.water.waterweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by water on 2017/3/20.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
